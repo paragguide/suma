83e9e7c3a03e9c701f8b670ebe88dc742a2609a5
paragguide <paragguide@yahoo.co.in>
2022-06-06 10:19:24 +0530

    my maven project

---
+++ src/main/java/utility/TestConfig.java
@@ -0,0 +1,22 @@
+
+package utility;
+
+public class TestConfig{
+
+
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "sumachitgupekar@gmail.com";
+	public static String password = "biburewibfplbdvz";
+	public static String[] to ={"paragguide@yahoo.co.in","sumachitgupekar@gmail.com","roliguide@gmail.com"};
+	public static String subject = "Test Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath= System.getProperty("user.dir")+"\\src\\test\\java\\report\\SeekerReportResults.html";
+	
+	public static String attachmentName="SeekerReportResults.html";
+	
+	
+	
+		
+}
